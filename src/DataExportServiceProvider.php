aa51a617b1aedd78073ba11cc9d78e8188ad9bd5
Lukas Juhas <lukas@worksome.com>
2021-08-19 14:13:18 +0200

    Add migrations to the provider

--- src/DataExportServiceProvider.php
+++ src/DataExportServiceProvider.php
@@ -5,6 +5,7 @@ namespace Worksome\DataExport;
 use Illuminate\Support\ServiceProvider;
 use Worksome\DataExport\Delivery\Contracts\Delivery;
 use Worksome\DataExport\Delivery\DeliveryManager;
+use Worksome\DataExport\Generator\Contracts\Generator;
 use Worksome\DataExport\Generator\GeneratorManager;
 use Worksome\DataExport\Processor\Contracts\Processor;
 use Worksome\DataExport\Processor\ProcessorManager;
@@ -16,5 +17,10 @@ class PayrollProvider extends ServiceProvider
         $this->app->bind(Processor::class, ProcessorManager::class);
         $this->app->bind(Generator::class, GeneratorManager::class);
         $this->app->bind(Delivery::class, DeliveryManager::class);
+    }
+
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '../database/migrations');
     }
 }
