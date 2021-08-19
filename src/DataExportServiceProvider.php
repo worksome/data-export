4f2518408dba1181904bbff55efaf70a2705de5a
Lukas Juhas <lukas@worksome.com>
2021-08-19 14:50:48 +0200

    Correction to migrations

--- src/DataExportServiceProvider.php
+++ src/DataExportServiceProvider.php
@@ -21,6 +21,6 @@ class DataExportServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '../database/migrations');
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 }
