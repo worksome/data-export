789705c339d2351f03310f6fde158964c2d0f583
Lukas Juhas <lukasjuhas@pm.me>
2022-12-20 15:48:58 -0500

    fix(exports): JIRA-8568 Fix timezones
    Whenever an export is created, the timezone being saved was wrong and
    resulted in moving the dates +-1 day.

--- tests/Feature/GraphQL/Mutations/CreateExportTest.php
+++ tests/Feature/GraphQL/Mutations/CreateExportTest.php
@@ -7,6 +7,7 @@ use Worksome\DataExport\Enums\ExportResponseStatus;
 use Worksome\DataExport\Events\ExportInitialised;
 use Worksome\DataExport\GraphQL\Mutations\CreateExport;
 use Worksome\DataExport\GraphQL\NullExportValidator;
+use Worksome\DataExport\Models\Export;
 use Worksome\DataExport\Services\CreateExport as CreateExportService;
 
 it('can create an export', function () {
@@ -37,4 +38,36 @@ it('can create an export', function () {
     Event::assertDispatched(ExportInitialised::class);
 
     expect($response['status'])->toBe(ExportResponseStatus::SUCCESS);
+
+    $export = Export::latest('created_at')->first()->refresh();
+    expect($export->args['dateFrom'])->toBe('2021-01-01');
+    expect($export->args['dateTo'])->toBe('2021-01-02');
+});
+
+it('can exports with correct dates', function () {
+    $args = [
+        'input' => [
+            'userId'      => 1,
+            'accountId'   => 1,
+            'accountType' => 'user',
+            'type'        => 'contract',
+            'generatorType' => 'csv',
+            'deliveries'    => [
+                ['type' => 'email', 'value' => 'john@doe.com'],
+            ],
+            'args' => [
+                'dateFrom' => '2022-01-01T23:00:00.000000Z',
+                'dateTo' => '2022-01-01T23:00:00.000000Z',
+            ],
+        ],
+    ];
+
+    $service = new CreateExportService();
+    $validator = new NullExportValidator();
+
+    (new CreateExport($service, $validator))->__invoke(null, $args);
+
+    $export = Export::latest('created_at')->first()->refresh();
+    expect($export->args['dateFrom'])->toBe('2022-01-01');
+    expect($export->args['dateTo'])->toBe('2022-01-01');
 });
